25f0cda920972dcb95760e6a7b58b28b695772af
kitsystemyou <kitsystemyou@gmail.com>
2022-11-06 05:24:10 +0900

    Refine code

--- sample.js
+++ sample.js
@@ -3,14 +3,13 @@ function getcat() {
     let rURL = "https://aws.random.cat/meow";
     let r = new XMLHttpRequest();
     r.open("GET", rURL);
-
     r.responseType = "json";
     r.send();
 
     r.onload = function() {
         let jsonObj = r.response;
         let img = document.getElementById("catimg");
-        console.log("link", jsonObj["file"]);
+        console.debug("link", jsonObj["file"]);
         img.src = jsonObj["file"];
     };
 }
