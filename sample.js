8f5acc3aa20c31778dd37df8f5d2cafd1db3e12c
kitsystemyou <kitsystemyou@gmail.com>
2022-11-06 05:21:42 +0900

    Refactor remove unused part

--- sample.js
+++ sample.js
@@ -7,24 +7,10 @@ function getcat() {
     r.responseType = "json";
     r.send();
 
-    // let r2 = new XMLHttpRequest();
-    // r.onload = function() {
-    //     let jsonObj1 = r.response;
-    //     console.log("get url", jsonObj1["file"]);
-    //     // r2.responseType = "json";
-    //     r2.open("GET", jsonObj1["file"]);
-    //     r2.setRequestHeader("Access-Control-Allow-Origin", "*");
-    //     // r2.setRequestHeader("Accept", "*/*");
-    //     r2.send();
-    // };
-
     r.onload = function() {
         let jsonObj = r.response;
         let img = document.getElementById("catimg");
         console.log("link", jsonObj["file"]);
         img.src = jsonObj["file"];
-        // img.style.display = "inline-block";
     };
-    console.log("end getcat");
 }
-// getcat();
